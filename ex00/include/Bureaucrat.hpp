b4bb49e76540e6926510a4b1aebe208771d679cb
stanislavlia <stanislavliashkov@gmail.com>
2024-08-23 13:47:02 +0700

    done with ex00

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -3,25 +3,39 @@
 
 #include <iostream>
 #include <exception>
-
+#include <string>
 
 class Bureaucrat
 {
-    private:
-        const std::string name;
-        unsigned int grade;
-    
+private:
+    const std::string _name;
+    unsigned int _grade; // grade 1 - highest; grade 150 - lowest (reverse direction)
+
+public:
+    Bureaucrat();
+    Bureaucrat(const std::string &name, unsigned int grade);
+    Bureaucrat(const Bureaucrat& other);
+    ~Bureaucrat();
+    Bureaucrat& operator=(const Bureaucrat& other);
+
+    const std::string& getName() const;
+    unsigned int getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
+
+    // Exception classes
+    class GradeTooHighException : public std::exception {
     public:
-        Bureaucrat();
-        Bureaucrat(std::string &name, unsigned int grade);
-        ~Bureaucrat();
-        Bureaucrat(const Bureaucrat& other);
-        const Bureaucrat& operator=(const Bureaucrat& other);
+        const char* what() const throw();  // Use 'throw()' for C++98 compatibility
+    };
 
-        const std::string& getName() const;
-        unsigned int getGrade();
-
+    class GradeTooLowException : public std::exception {
+    public:
+        const char* what() const throw();  // Use 'throw()' for C++98 compatibility
+    };
 };
 
+// Non-member function for operator<<
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& obj);
 
 #endif
---
+++ ex00/src/Bureaucrat.cpp
@@ -0,0 +1,76 @@
+#include "Bureaucrat.hpp"
+
+//=====================CONSTRUCTORS==========================
+
+Bureaucrat::Bureaucrat() : _name("NoName"), _grade(150) //lowest grade by default
+{
+
+};
+
+Bureaucrat::Bureaucrat(const std::string& name, unsigned int grade) : _name(name), _grade(grade)
+{
+    //Need to throw exception if out of range!
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+};
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
+{
+    //Need to throw exception if out of range!
+    if (other._grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (other._grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+};
+
+Bureaucrat::~Bureaucrat()
+{
+
+};
+
+
+const std::string& Bureaucrat::getName() const
+{
+    return _name;
+};
+
+unsigned int Bureaucrat::getGrade() const
+{
+    return _grade;
+};
+
+void    Bureaucrat::incrementGrade()
+{
+    if (_grade - 1 < 1)
+        throw Bureaucrat::GradeTooHighException();
+
+    std::cout << "Increment grade for "<< getName() << std::endl;
+    _grade -= 1;
+};
+
+void Bureaucrat::decrementGrade()
+{
+    if (_grade + 1 > 150)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Decrement grade for "<< getName() << std::endl;
+    _grade += 1;
+};
+
+//====================EXCEPTION IMPLEMENTATION===============================
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "The grade is too high. The highest possible grade is 1\n";
+};
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "The grade is too low. The lowest possible grade is 150\n";
+};
+
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& obj)
+{
+    o << obj.getName() << ", bureaucrat grade " << obj.getGrade() << "." << std::endl;
+    return o;
+};
