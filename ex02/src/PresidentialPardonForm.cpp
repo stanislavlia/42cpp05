374105d6fb7ccbb393bea4872ef2530cac3d9612
stanislavlia <stanislavliashkov@gmail.com>
2024-08-26 23:56:03 +0700

    ex02 is done

---
+++ ex02/src/PresidentialPardonForm.cpp
@@ -0,0 +1,37 @@
+#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string& target)
+    : AForm("PresidentialPardonForm", 25, 5), _target(target)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other)
+    : AForm(other), _target(other._target)
+{
+}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other)
+{
+    if (this != &other)
+    {
+        AForm::operator=(other);
+        _target = other._target;
+    }
+    return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void PresidentialPardonForm::execute(const Bureaucrat& executor) const
+{
+    if (!isSigned())
+        throw AForm::NotSignedException();
+    if (executor.getGrade() > getGradeToExec())
+        throw AForm::GradeTooLowException();
+
+    std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,20 +1,43 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 #include "Bureaucrat.hpp"
 
 
 int main()
 {
+    //=======employees==========
     Bureaucrat sergey("Sergey", 5);
-    Bureaucrat jack("Jack", 90);
+    Bureaucrat jack("Jack", 40);
+    Bureaucrat kane("Kane", 135);
+    Bureaucrat kassy("Kassy", 90);
 
-    ShrubberyCreationForm   shubform("Jack");
-    RobotomyRequestForm     rbform("Sergey");
+    //=======forms=================
+    ShrubberyCreationForm   sb_form("Kane");
+    RobotomyRequestForm     rb_form("Jack");
+    PresidentialPardonForm  prs_form("Sergey");
 
-    sergey.signForm(shubform);
-    sergey.signForm(rbform);
-    jack.executeForm(shubform);
-    sergey.executeForm(rbform);
+    std::cout << "=========TRY TO EXECUTE NOT SIGNED FORMS===============\n";
+    //try to execute not signed form
+    sergey.executeForm(sb_form);
+    kane.executeForm(rb_form);
+
+    std::cout << "=========SIGN FORMS===============\n";
+    //now, let's sign them
+    sergey.signForm(prs_form);
+    jack.signForm(rb_form);
+    kassy.signForm(sb_form);
+    
+    std::cout << "=========TRY TO EXECUTE===============\n";
+    //try execute without needed grade
+    kane.executeForm(prs_form);
+    kassy.executeForm(prs_form);
+
+    std::cout << "=========EXECUTE FORMS CORRECTLY===============\n";
+    //execute forms appropriately
+    kane.executeForm(sb_form);
+    jack.executeForm(rb_form);
+    sergey.executeForm(prs_form);
 
     return 0;
 };
